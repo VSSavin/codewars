07ef7823c3784b9eb14ebde7c1e8ce8cff7ab815
svs-devel <example@example.com>
2020-07-03 11:26:41 +0300

    codewars: simpleAssembler task commit

---
+++ codewars/src/main/java/com/codewars/kata/train/simpleAssembler/SimpleAssembler.java
@@ -0,0 +1,125 @@
+package com.codewars.kata.train.simpleAssembler;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by valera2 on 03.07.20.
+ */
+public class SimpleAssembler {
+    static Map<String, Integer> out = new HashMap<String, Integer>();
+    public static Map<String, Integer> interpret(String[] program){
+        out.clear();
+        System.out.println("Program:\r\n----------\r\n" + Arrays.toString(program));
+        System.out.println("----------");
+
+        for (int i = 0; i < program.length; i++)
+        {
+            String[] data = program[i].split(" ");
+            try
+            {
+                int jmp = processingInstruction(data);
+                if (jmp != 0) i = i + jmp - 1;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+        return out;
+    }
+
+    private static int processingInstruction(String[] instruction) throws Exception
+    {
+        int jmp = 0;
+        if (instruction.length > 0)
+        {
+            switch(instruction[0])
+            {
+                case "jnz":
+                    try
+                    {
+                        if (!isIntNumeric(instruction[1]))
+                        {
+                            if (out.get(instruction[1]) != 0) jmp = Integer.parseInt(instruction[2]);
+                        }
+                        else
+                        {
+                            if (Integer.parseInt(instruction[1]) != 0) jmp = Integer.parseInt(instruction[2]);
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception("instruction: " + Arrays.toString(instruction) + " processing error" , e);
+                    }
+
+                    break;
+                case "mov":
+                    try
+                    {
+                        if (isIntNumeric(instruction[2]))
+                        {
+                            out.put(instruction[1], Integer.parseInt(instruction[2]));
+                        }
+                        else
+                        {
+                            out.put(instruction[1], out.get(instruction[2]));
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception("instruction: " + Arrays.toString(instruction) + " processing error" , e);
+                    }
+
+                    break;
+                case "inc":
+                    try
+                    {
+                        out.put(instruction[1], out.get(instruction[1]) + 1);
+                    }
+                    catch (NullPointerException npe)
+                    {
+                        System.out.println("Unknown register: " + instruction[1]);
+                    }
+
+                    break;
+
+                case "dec":
+                    try
+                    {
+                        out.put(instruction[1], out.get(instruction[1]) - 1);
+                    }
+                    catch (NullPointerException npe)
+                    {
+                        System.out.println("Unknown register: " + instruction[1]);
+                    }
+
+                    break;
+
+                default:
+                    throw new Exception("Unsupported command: " + instruction[0]);
+            }
+        }
+
+        return jmp;
+    }
+
+    private static boolean isIntNumeric(String str)
+    {
+        boolean ret = true;
+        try
+        {
+            Integer.parseInt(str);
+        }
+        catch (NumberFormatException nfe)
+        {
+            ret = false;
+        }
+
+        return ret;
+    }
+}
---
+++ codewars/src/test/java/com/codewars/kata/train/simpleAssembler/SolutionTest.java
@@ -0,0 +1,51 @@
+package com.codewars.kata.train.simpleAssembler;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by valera2 on 03.07.20.
+ */
+public class SolutionTest {
+    @Test
+    public void simple1() {
+        String[] program = new String[]{"mov a 5","inc a","dec a","dec a","jnz a -1","inc a"};
+        Map<String, Integer> out = new HashMap<String, Integer>();
+        out.put("a", 1);
+        assertEquals(out, SimpleAssembler.interpret(program));
+    }
+
+    @Test
+    public void simple2() {
+        String[] program = new String[]{"mov a -10","mov b a","inc a","dec b","jnz a -2"};
+        Map<String, Integer> out = new HashMap<String, Integer>();
+        out.put("a", 0);
+        out.put("b", -20);
+        assertEquals(out, SimpleAssembler.interpret(program));
+    }
+
+    @Test
+    public void complex1() {
+        String[] program = new String[]{"mov a 1", "mov b 1", "mov c 0", "mov d 26", "jnz c 2", "jnz 1 5", "mov c 7", "inc d", "dec c", "jnz c -2", "mov c a", "inc a", "dec b", "jnz b -2", "mov b c", "dec d", "jnz d -6", "mov c 18", "mov d 11", "inc a", "dec d", "jnz d -2", "dec c", "jnz c -5"};
+        Map<String, Integer> out = new HashMap<String, Integer>();
+        out.put("a", 318009);
+        out.put("b", 196418);
+        out.put("c", 0);
+        out.put("d", 0);
+        assertEquals(out, SimpleAssembler.interpret(program));
+    }
+
+    @Test
+    public void complex2() {
+        String[] program = new String[]{"mov d 100", "dec d", "mov b d", "jnz b -2", "inc d", "mov a d", "jnz 5 10", "mov c a"};
+        Map<String, Integer> out = new HashMap<String, Integer>();
+        out.put("a", 1);
+        out.put("b", 0);
+        out.put("d", 1);
+        assertEquals(out, SimpleAssembler.interpret(program));
+    }
+}
